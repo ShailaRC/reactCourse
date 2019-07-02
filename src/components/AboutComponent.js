2b1ffdb3346218ed58cb57f689f143c8829ed995
Shaila <shaila.rcabrera@gmail.com>
2019-07-02 18:26:39 -0400

    Task2 Task3 Assignment2

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,11 +2,30 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+function RenderLeader ({leader}) {
+    return(
+        <div key={leader.id} className="col-12">
+            <Media  tag="li">
+                <Media left middle>
+                    <Media object src={leader.image} alt={leader.name} />
+                </Media>
+                <Media body className="ml-5">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.designation}</p>
+                    <p>{leader.description}</p>
+                </Media>
+
+            </Media>
+        </div>
+        
+    );
+}
+
 function About(props) {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <p>Leader {leader.name}</p>
+            <RenderLeader leader={leader} />
         );
     });
 
@@ -64,11 +83,12 @@ function About(props) {
                 <div className="col-12">
                     <h2>Corporate Leadership</h2>
                 </div>
-                <div className="col-12">
+                {leaders}
+                {/* <div className="col-12">
                     <Media list>
                         {leaders}
                     </Media>
-                </div>
+                </div> */}
             </div>
         </div>
     );
