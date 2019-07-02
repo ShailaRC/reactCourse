54e294a11213018262a04604a95b57c6d3d5d878
Shaila <shaila.rcabrera@gmail.com>
2019-07-02 18:28:01 -0400

    Clean Assignment2

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -84,11 +84,6 @@ function About(props) {
                     <h2>Corporate Leadership</h2>
                 </div>
                 {leaders}
-                {/* <div className="col-12">
-                    <Media list>
-                        {leaders}
-                    </Media>
-                </div> */}
             </div>
         </div>
     );
