778d9eb009e217b47798edf8cc0624a77c5cb8e1
Shaila <shaila.rcabrera@gmail.com>
2019-07-05 11:20:06 -0400

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -25,7 +25,7 @@ function RenderDish({ dish }) {
   }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
   if (comments != null) {
     const listComments = comments.map((comment) => {
       return (
@@ -41,12 +41,12 @@ function RenderComments({ comments }) {
       <div className="col-12 col-md-5 m-1">
         <h4>Comments</h4>
         {listComments}
-        <CommentForm />
+        <CommentForm  dishId={dishId} addComment={addComment}/>
       </div>
     );
   } else {
     return (
-      <div><CommentForm /></div>
+      <div></div>
     );
   }
 }
@@ -69,7 +69,7 @@ const DishDetail = (props) => {
       </div>
       <div className="row">
         <RenderDish dish={props.dish} />
-        <RenderComments comments={props.comments} />
+        <RenderComments comments={props.comments}  addComment={props.addComment} dishId={props.dish.id}/>
       </div>
     </div>
   );
@@ -100,8 +100,10 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+      this.toggleModal(); 
+      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment); //from video, not from excercise
+        // console.log('Current State is: ' + JSON.stringify(values));
+        // alert('Current State is: ' + JSON.stringify(values));
         // event.preventDefault();
     }
 
