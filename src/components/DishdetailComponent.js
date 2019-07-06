5be9711afa95919128712e9b6c897d1423d36a7b
Shaila <shaila.rcabrera@gmail.com>
2019-07-06 19:29:53 -0400

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -31,7 +31,7 @@ function RenderDish({ dish }) {
   }
 }
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
   if (comments != null) {
     const listComments = comments.map((comment) => {
       return (
@@ -47,7 +47,7 @@ function RenderComments({ comments, addComment, dishId }) {
       <div className="col-12 col-md-5 m-1">
         <h4>Comments</h4>
         {listComments}
-        <CommentForm  dishId={dishId} addComment={addComment}/>
+        <CommentForm  dishId={dishId} postComment={postComment}/>
       </div>
     );
   } else {
@@ -80,7 +80,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal(); 
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment); //from video, not from excercise
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment); //from video, not from excercise
       // console.log('Current State is: ' + JSON.stringify(values));
       // alert('Current State is: ' + JSON.stringify(values));
       // event.preventDefault();
@@ -187,7 +187,7 @@ const DishDetail = (props) => {
         </div>
         <div className="row">
           <RenderDish dish={props.dish} />
-          <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} />
+          <RenderComments comments={props.comments} postComment={props.postComment} dishId={props.dish.id} />
         </div>
       </div>
     );
